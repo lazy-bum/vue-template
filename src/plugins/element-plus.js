47dc2e50f1ed486fd4b74a502cab283d656dc3f8
lazybum <318640675@qq.com>
2021-05-28 23:54:52 +0800

    fix: 修复build后国际化报错问题

--- src/plugins/element-plus.js
+++ src/plugins/element-plus.js
@@ -1,11 +1,10 @@
 import 'element-plus/packages/theme-chalk/src/base.scss'
 import 'dayjs/locale/zh-cn'
-import locale from 'element-plus/lib/locale'
 import lang from 'element-plus/lib/locale/lang/zh-cn'
-import { ElButton, ElSpace, ElRow, ElCol } from 'element-plus';
+import { locale, ElButton, ElSpace, ElRow, ElCol } from 'element-plus';
 
 export default app => {
-  locale.use(lang)
+  import.meta.env.MODE === 'production' ? locale.use(lang) : locale(lang)
   app.component(ElButton.name, ElButton)
   app.component(ElSpace.name, ElSpace)
   app.component(ElRow.name, ElRow)
